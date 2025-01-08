f30dd7fe43b4a5cac75ee7bce9b50aefc65f2eee
lotte0316 <mm621648@gmail.com>
2025-01-08 21:42:08 +0800

    update認識新變數

--- Assets/scripts/Class_1_Variable.cs
+++ Assets/scripts/Class_1_Variable.cs
@@ -2,8 +2,8 @@
 /*多行註解*/
 //引用Unity遊戲引擎的韓式庫(開發遊戲的程式集合，命名空間)
 using UnityEngine;
-//修飾詞類別關鍵字(腳本)類別名稱腳本名稱
-public class Class_1_Variable
+//修飾詞類別關鍵字(腳本)類別名稱腳本名稱//MonoBehaviour控制遊戲物件並可與之互動
+public class Class_1_Variable:MonoBehaviour 
 
 {
     //程式內的括號都是成對出現{} () "" <> [] ''
@@ -12,6 +12,9 @@ public class Class_1_Variable
     //變數語法:變數資料類型 變數名稱;
     //資料類型告訴記憶體可放入那些資料
     //變數名稱:習慣用小寫開頭命名，駝峰式命名，規則與腳本名稱相同
-    int coin;
+    //公開:充許其他類別存取public
+    //私人:禁止其他類別存取private(預設)
+    public int coin=500;
+    private int value=3;
 }
 //類別外，不要將程式寫在這
---
+++ Assets/scripts/Class_2_DataType.cs
@@ -0,0 +1,29 @@
+﻿using UnityEngine;
+//三條斜線是摘要(XML 語言)用來做簡短說明，可加在類別與變數上方
+/// <summary>
+/// 認識資料類型
+/// </summary>
+
+public class Class_2_DataType:MonoBehaviour
+{
+    //變數語法 :修飾詞 資料類型 便是名稱 只並 預設值;
+    //四大常用資料類型
+    //整數int 浮點數float 字串string 布林值 bool->true||false兩值
+    public int count = 7;
+    public float moveSpeed = 3.5f;   //+f大小寫皆可
+    public float trunSpeed = 30.5F;
+    
+    public bool isDead=false;  //結果:否
+    public bool gameOver=true; //解果:是
+
+    public long item = 99999;//64位元
+    public ushort item1 = 10000;//16位元
+    public byte item2 = 100;//8位元
+
+    public char item3 = 'h';//單一字元''
+    public string character = "蓋倫";//字串""
+
+    //public byte item4 = 256;//溢位導致錯誤
+
+
+}
