8521056a9a4043953aeb108f8c586a097a5b58d3
lotte0316 <mm621648@gmail.com>
2025-01-08 22:34:25 +0800

    update1.2作業完成

---
+++ Assets/scripts/練習_1_變數.cs
@@ -0,0 +1,11 @@
+using UnityEditor.Search;
+using UnityEngine;
+
+public class 練習_1_變數 : MonoBehaviour
+{
+    public long price = 999;
+    private float jumpheight=77.5f;
+    public string arms = "三項之力";
+    private bool 是否獲得龍魂 = true;
+  
+}
---
+++ Assets/scripts/練習_2_資料類型.cs
@@ -0,0 +1,8 @@
+using UnityEngine;
+
+public class 練習_2_資料類型 : MonoBehaviour
+{
+    public float high = 7.7f;
+    public double high1 = 0.12345d;
+    decimal high2 = 1.123456789m;
+}
