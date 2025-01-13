7e8b348bd4af6d370ad84ca0479f1f027ec37900
lotte0316 <mm621648@gmail.com>
2025-01-13 19:13:23 +0800

    update 完成作業練習

---
+++ Assets/practice/Practice_1_Variable.cs
@@ -0,0 +1,11 @@
+using UnityEditor.Search;
+using UnityEngine;
+
+public class practice_1_Variable : MonoBehaviour
+{
+    public long price = 999;
+    private float jumpheight = 77.5f;
+    public string arms = "三項之力";
+    private bool getDragonsoul = true;
+
+}
--- Assets/practice/Practice_2_DataType.cs
+++ Assets/practice/Practice_2_DataType.cs
@@ -1,8 +1,8 @@
 using UnityEngine;
 
-public class 練習_2_資料類型 : MonoBehaviour
+public class Practice_2_DataType : MonoBehaviour
 {
     public float high = 7.7f;
     public double high1 = 0.12345d;
-    decimal high2 = 1.123456789m;
+    public decimal high2 = 1.123456789m;
 }
