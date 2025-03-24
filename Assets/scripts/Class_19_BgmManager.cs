22a17c2d37ec20aec61aee39afe0fb96140a92c8
lotte0316 <mm621648@gmail.com>
2025-03-24 21:54:49 +0800

    Updata 認識LinQ

---
+++ Assets/scripts/Class_19_BgmManager.cs
@@ -0,0 +1,43 @@
+﻿using UnityEngine;
+using Yao.Tools;
+
+namespace Yao.Class_19.Event
+{
+    public class Class_19_BgmManager:MonoBehaviour
+    {
+        [SerializeField]
+        private Class_19_Event class_19_event;
+
+        //步驟3.訂閱事件(Unity 習慣在 Awake 或Start 訂閱)
+        private void Awake()
+        {
+            //事件:輸入+=可以使用Tab自動完成>命名方法>Enter完成
+            class_19_event.onDead += ChangeBgm;
+            class_19_event.onDeadAction += ChangeBgmViaHp;
+            class_19_event.onEvent+= ChangeBgmEvent;
+            class_19_event.onEventWithHp += ChangeBgmeventWithHp;
+        }
+
+        private void ChangeBgmeventWithHp(object sender, float e)
+        {
+            LogSystem.LogWithColor($"發送事件者:{sender},參數:{e}", "#ff3");
+        }
+        private void ChangeBgmEvent(object sender, System.EventArgs e)
+        {
+            LogSystem.LogWithColor($"發送事件者:{sender},參數:{e}", "#ff3");
+        }
+
+        private void ChangeBgmViaHp(string arg1, float arg2)
+        {
+            LogSystem.LogWithColor($"{arg1},|血量:{arg2}", "#78f");
+            if (arg2 <= -5) LogSystem.LogWithColor("變更為遊戲結束音樂", "#777");
+        
+        }
+
+        private void ChangeBgm()
+        {
+            LogSystem.LogWithColor("變更音樂", "#3f3");
+        
+        }
+    }
+}
---
+++ Assets/scripts/Class_19_Lin Q.cs
@@ -0,0 +1,61 @@
+﻿using UnityEngine;
+using System.Linq;//引用LinQ命名空間
+using Yao.Tools;
+
+namespace Yao.Class_19.LinQ
+{
+    /// <summary>
+    /// LinQ 整合查詢語言 Language Intrgrated Query
+    /// 查詢資料庫，所有程式語言共用技術
+    /// </summary>
+    public class Class_19_LinQ : MonoBehaviour
+    {
+        public int[] scores = { 90,80,77,88,93,1,5,10,35 };
+
+        private void Awake()
+        {
+            //使用 LinQ的關鍵字查詢資料
+            //起手式:
+            //fromy 資料來源
+            //select選取結果
+            //說明:選取scores內的每一筆分數
+            var queryAllData=from score in scores
+                             select score;
+            foreach (var item in queryAllData)
+            { 
+                LogSystem.LogWithColor(item,"#3f3");
+            }
+            //情境:希望找到大於60分的玩家分數並且排序
+            //where 篩選的條件
+            //orderby 排序(預設從小到大)descending由大到小
+            var querySisty=from score in scores
+                           where score >=60
+                           orderby score descending
+                           select score;
+            foreach (var item in querySisty)
+            {
+                LogSystem.LogWithColor(item, "#ff3");
+            
+            }
+
+            //情境:篩選出分數為偶數的資料與奇數
+            //grouop by
+            var queryGrouop = from score in scores
+                              group score by score % 2;
+
+            foreach (var group in queryGrouop)
+            {
+                LogSystem.LogWithColor(group, "#f11");
+                foreach (var item in group)
+                {
+                    LogSystem.LogWithColor(item, "#79f");
+                }
+                LogSystem.LogWithColor("------群組分隔線-------","#79f");
+
+            }
+
+
+        }
+
+    }
+}
